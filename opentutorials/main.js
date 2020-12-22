3c30e768d084f879c41667bd426dc5df05ea7b62
cloudlee <cloudlee@MacBook-Pro-9.local>
2020-12-22 15:20:04 +0900

    Add: Express CRUD - with OpenTutorials

--- opentutorials/main.js
+++ opentutorials/main.js
@@ -1,8 +1,181 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var path = require("path");
+var sanitizeHtml = require("sanitize-html");
+var template = require("./lib/template.js");
+const { response } = require("express");
+var qs = require("querystring");
+
+//routing-> path 마다 응답 바꿔주는거임
+// if문을 통해 실행했던것을 get과 send를 통해서 구현하는 것임
+//if문 중첩을 피할 수 있다 :)
+app.get("/", (request, response) => {
+  fs.readdir("./data", function (error, filelist) {
+    var title = "Welcome";
+    var description = "Hello, Node.js";
+    var list = template.list(filelist);
+    var html = template.HTML(
+      title,
+      list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    response.send(html);
+  });
+});
+
+// 상세보기 구현
+app.get("/page/:pageId", function (request, response) {
+  fs.readdir("./data", function (error, filelist) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update/${sanitizedTitle}">update</a>
+          <form action="/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
+});
+
+//생성기능
+app.get("/create", (req, res) => {
+  fs.readdir("./data", function (error, filelist) {
+    var title = "WEB - create";
+    var list = template.list(filelist);
+    //form action="/create_process" 은 최상위 디렉토리에서 create_process경로를 찾는것이다 '/'를 기억하자 :)
+    var html = template.HTML(
+      title,
+      list,
+      `
+        <form action="/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `,
+      ""
+    );
+    res.send(html);
+  });
+});
+
+app.post("/create_process", (request, response) => {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+      response.writeHead(302, { Location: `/?id=${title}` });
+      response.end();
+    });
+  });
+});
+
+//update
+app.get("/update/:pageId", (request, response) => {
+  fs.readdir("./data", function (error, filelist) {
+    let filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = filteredId;
+      var list = template.list(filelist);
+      var html = template.HTML(
+        title,
+        list,
+        `
+          <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+      response.send(html);
+    });
+  });
+});
+
+app.post("/update_process", (request, response) => {
+  let body = "";
+  request.on("data", (data) => {
+    body = body + data;
+  });
+  request.on("end", () => {
+    let post = qs.parse(body);
+    let id = post.id;
+    let title = post.title;
+    let description = post.description;
+
+    fs.rename(`data/${id}`, `data/${title}`, (error) => {
+      fs.writeFile(`data/${title}`, description, "utf8", (err) => {
+        response.redirect(`/?id=${title}`);
+      });
+    });
+  });
+});
+
+//delete
+app.post("/delete_process", (request, response) => {
+  let body = "";
+  request.on("data", (data) => {
+    body = body + data;
+  });
+  request.on("end", () => {
+    let post = qs.parse(body);
+    let id = post.id;
+    let filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, (error) => {
+      response.redirect("/");
+    });
+  });
+});
+
+//route paramter를 통해 값 전달
+
+// app.get("/", function (req, res) {
+//   return res.send("Hello World!");
+// });
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`);
+});
+
+///Under code is written without 'express';
+/*
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
 var template = require("./lib/template.js");
+var path = require("path");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -25,27 +198,24 @@ var app = http.createServer(function (request, response) {
       });
     } else {
       fs.readdir("./data", function (error, filelist) {
-        fs.readFile(
-          `data/${queryData.id}`,
-          "utf8",
-          function (err, description) {
-            var title = queryData.id;
-            var list = template.list(filelist);
-            var html = template.HTML(
-              title,
-              list,
-              `<h2>${title}</h2>${description}`,
-              ` <a href="/create">create</a>
+        var filteredId = path.parse(queryData.id).base; // 경로 보안화 시킴 ../같은거 .. 해놓지 않으면 경로타고 다 볼 수 있기 때문
+        fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+          var title = queryData.id;
+          var list = template.list(filelist);
+          var html = template.HTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`,
+            ` <a href="/create">create</a>
                 <a href="/update?id=${title}">update</a>
                 <form action="delete_process" method="post">
                   <input type="hidden" name="id" value="${title}">
                   <input type="submit" value="delete">
                 </form>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          }
-        );
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
       });
     }
   } else if (pathname === "/create") {
@@ -149,3 +319,7 @@ var app = http.createServer(function (request, response) {
 app.listen(3000, () => {
   console.log("port 3000");
 });
+
+//출력보안은 추후에 보자
+
+*/
